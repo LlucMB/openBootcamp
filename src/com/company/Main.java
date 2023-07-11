105beb5ea6f729d53191645fab5bf9482c80074e
LlucMB <lluc.martinez@outlook.es>
2023-07-11 22:40:59 +0200

    se agrega ejercicio

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            int resultado = suma(50, 30, 20);
+
+            System.out.println(resultado);
+        }
+
+        public static int suma(int a, int b, int c) {
+            return a + b + c;
+        }
+
+}
